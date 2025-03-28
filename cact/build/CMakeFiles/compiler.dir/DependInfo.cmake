cbe0df2300dab7900d28824ad8c9a17953925bbf
ucaszjw <zhangjiawei2004@126.com>
2025-03-28 11:11:18 +0800

    build hello.g4 and compiler

---
+++ cact/build/CMakeFiles/compiler.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/zhangjiawei/Desktop/cact/grammar/HelloBaseVisitor.cpp" "CMakeFiles/compiler.dir/grammar/HelloBaseVisitor.cpp.o" "gcc" "CMakeFiles/compiler.dir/grammar/HelloBaseVisitor.cpp.o.d"
+  "/Users/zhangjiawei/Desktop/cact/grammar/HelloLexer.cpp" "CMakeFiles/compiler.dir/grammar/HelloLexer.cpp.o" "gcc" "CMakeFiles/compiler.dir/grammar/HelloLexer.cpp.o.d"
+  "/Users/zhangjiawei/Desktop/cact/grammar/HelloParser.cpp" "CMakeFiles/compiler.dir/grammar/HelloParser.cpp.o" "gcc" "CMakeFiles/compiler.dir/grammar/HelloParser.cpp.o.d"
+  "/Users/zhangjiawei/Desktop/cact/grammar/HelloVisitor.cpp" "CMakeFiles/compiler.dir/grammar/HelloVisitor.cpp.o" "gcc" "CMakeFiles/compiler.dir/grammar/HelloVisitor.cpp.o.d"
+  "/Users/zhangjiawei/Desktop/cact/src/main.cpp" "CMakeFiles/compiler.dir/src/main.cpp.o" "gcc" "CMakeFiles/compiler.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cact/build/CMakeFiles/compiler.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler.dir/grammar/HelloBaseVisitor.cpp.o"
+  "CMakeFiles/compiler.dir/grammar/HelloBaseVisitor.cpp.o.d"
+  "CMakeFiles/compiler.dir/grammar/HelloLexer.cpp.o"
+  "CMakeFiles/compiler.dir/grammar/HelloLexer.cpp.o.d"
+  "CMakeFiles/compiler.dir/grammar/HelloParser.cpp.o"
+  "CMakeFiles/compiler.dir/grammar/HelloParser.cpp.o.d"
+  "CMakeFiles/compiler.dir/grammar/HelloVisitor.cpp.o"
+  "CMakeFiles/compiler.dir/grammar/HelloVisitor.cpp.o.d"
+  "CMakeFiles/compiler.dir/src/main.cpp.o"
+  "CMakeFiles/compiler.dir/src/main.cpp.o.d"
+  "compiler"
+  "compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
